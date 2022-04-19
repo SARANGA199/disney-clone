bde944f4cbd1502cf4f54579e9f728ef2d698ec7
Piumika Saranga <IT20260460@my.sliit.lk>
2022-04-19 13:56:26 +0530

    create Header v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 import Login from './components/Login';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       
      <Router>
+       <Header />
        <Switch>
           
           <Route exact path="/" ><Login /></Route>
